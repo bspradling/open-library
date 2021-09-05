c4dfbad35d0a861242356339feae28eea78ad489
Brett Spradling <brett@spradling.me>
2021-09-05 15:47:19 -0700

    adding ability to login

--- src/clients/tests/books.rs
+++ src/clients/tests/books.rs
@@ -1,16 +1,19 @@
 use crate::models::books::{BookIdentifier, BookIdentifierKey};
 use crate::OpenLibraryClient;
+use std::error::Error;
 
 #[tokio::test]
-async fn verify_search_call() {
+async fn verify_search_call() -> Result<(), Box<dyn Error>> {
     //TODO: get wiremock working for unit tests
-    let client = OpenLibraryClient::new().unwrap();
+    let client = OpenLibraryClient::builder().build()?;
     let results = client
         .books
         .search(vec![
             &BookIdentifier::from(BookIdentifierKey::ISBN, "0451526538".to_string()),
             &BookIdentifier::from(BookIdentifierKey::GoodReads, "0451526538".to_string()),
         ])
-        .await;
-    assert_eq!(results.unwrap().len(), 1)
+        .await?;
+
+    assert_eq!(results.len(), 1);
+    Ok(())
 }
