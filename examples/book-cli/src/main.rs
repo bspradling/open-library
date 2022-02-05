f7a2ad5a0ea1726f0e5a8cb0c11ee4c042e0a768
Brett Spradling <brett@spradling.me>
2022-02-05 08:22:48 -0800

    fixing compilation errors

--- examples/book-cli/src/main.rs
+++ examples/book-cli/src/main.rs
@@ -1,7 +1,7 @@
 use crate::arguments::Arguments;
 use anyhow::Result;
-use open_library::models::books::{BibliographyKey, BookIdentifier};
-use open_library::models::Identifier;
+use open_library::models::books::BibliographyKey::{ISBN, LCCN, OCLC, OLID};
+use open_library::models::books::{BibliographyKey, BookIdentifierKey};
 use open_library::OpenLibraryClient;
 use structopt::StructOpt;
 use tokio;
@@ -13,29 +13,12 @@ async fn main() -> Result<()> {
     let arguments = Arguments::from_args();
     let client = OpenLibraryClient::builder().build()?;
 
-    let isbn = arguments.isbn().into_iter().map(|id| Identifier {
-        resource: BookIdentifier::from_isbn(&id).unwrap(),
-        identifier: id,
-    });
-    let oclc = arguments.oclc().into_iter().map(|id| Identifier {
-        resource: BookIdentifier::OhioCollegeLibraryCenter,
-        identifier: id,
-    });
-    let lccn = arguments.lccn().into_iter().map(|id| Identifier {
-        resource: BookIdentifier::LibraryOfCongress,
-        identifier: id,
-    });
-    let olid = arguments.olid().into_iter().map(|id| Identifier {
-        resource: BookIdentifier::OpenLibrary,
-        identifier: id,
-    });
+    let isbn = arguments.isbn().into_iter().map(ISBN);
+    let oclc = arguments.oclc().into_iter().map(OCLC);
+    let lccn = arguments.lccn().into_iter().map(LCCN);
+    let olid = arguments.olid().into_iter().map(OLID);
 
-    let identifiers: Vec<BibliographyKey> = isbn
-        .chain(oclc)
-        .chain(lccn)
-        .chain(olid)
-        .map(|id| BibliographyKey::from_identifier(id).unwrap())
-        .collect();
+    let identifiers: Vec<BibliographyKey> = isbn.chain(oclc).chain(lccn).chain(olid).collect();
 
     println!("{:?}", client.books.search(&identifiers).await?);
     Ok(())
